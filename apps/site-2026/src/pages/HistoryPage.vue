ef7fc8f93eba616d07a04fe605e01fe3276fadcb
Sean20405 <sean20405.cs11@nycu.edu.tw>
2026-01-28 16:01:44 +0800

    refactor (history): move css property to .vue

--- apps/site-2026/src/pages/HistoryPage.vue
+++ apps/site-2026/src/pages/HistoryPage.vue
@@ -1,45 +1,40 @@
 <template>
   <div class="history-page">
-    <div class="row">
-      <div class="col">
-        <h1>歷史紀錄</h1>
-        <div class="title-bar"/>
-        <Loading v-if="isLoading"/>
-        <div class="history-center" v-if="!isLoading">
-          <table class="record history-table">
-            
-            <thead>
-              <tr>
-                <th class="head" width="50">年度</th>
-                <th class="head" width="50">勝校</th>
-                <th class="head">比數</th>
-                <th class="head" v-for="item in itemList" :key="item.id">{{ item.name }}</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="archive in archiveList" :key="archive.year.year">
-                <td>{{ archive.year.year - 1911 }}<br>{{ archive.year.short }}</td>
-                <template v-if="archive.year.outcome == 'stop'">
-                  <td :colspan="itemList.length + 2">{{ getOutcome(archive.year.outcome) }}</td>
-                </template>
-                <template v-else-if="archive.year.outcome == 'friendly'">
-                  <td colspan="2">{{ getOutcome(archive.year.outcome) }}</td>
-                  <td v-for="row in archive.archive"  :class="'history-table__verdict history-table__verdict--' + row.outcome" :key="row.id">
-                    {{ getSchool(row.outcome) }}
-                  </td>
-                </template>
-                <template v-else>
-                  <td :class="'history-table__verdict history-table__verdict--' + archive.year.outcome">{{ getOutcome(archive.year.outcome, archive.year.year) }}</td>
-                  <td>{{ getScore(archive.year.outcome, archive.year.score_nctu, archive.year.score_nthu) }}</td>
-                  <td v-for="row in archive.archive" :class="'history-table__verdict history-table__verdict--' + row.outcome" :key="row.id">
-                    {{ getSchool(row.outcome) }}
-                  </td>
-                </template>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+    <h1>歷史紀錄</h1>
+    <div class="title-bar"/>
+    <Loading v-if="isLoading"/>
+    <div class="history-center" v-if="!isLoading">
+      <table class="record history-table">
+        <thead>
+          <tr>
+            <th class="head" width="50">年度</th>
+            <th class="head" width="50">勝校</th>
+            <th class="head">比數</th>
+            <th class="head" v-for="item in itemList" :key="item.id">{{ item.name }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="archive in archiveList" :key="archive.year.year">
+            <td>{{ archive.year.year - 1911 }}<br>{{ archive.year.short }}</td>
+            <template v-if="archive.year.outcome == 'stop'">
+              <td :colspan="itemList.length + 2">{{ getOutcome(archive.year.outcome) }}</td>
+            </template>
+            <template v-else-if="archive.year.outcome == 'friendly'">
+              <td colspan="2">{{ getOutcome(archive.year.outcome) }}</td>
+              <td v-for="row in archive.archive"  :class="'history-table__verdict history-table__verdict--' + row.outcome" :key="row.id">
+                {{ getSchool(row.outcome) }}
+              </td>
+            </template>
+            <template v-else>
+              <td :class="'history-table__verdict history-table__verdict--' + archive.year.outcome">{{ getOutcome(archive.year.outcome, archive.year.year) }}</td>
+              <td>{{ getScore(archive.year.outcome, archive.year.score_nctu, archive.year.score_nthu) }}</td>
+              <td v-for="row in archive.archive" :class="'history-table__verdict history-table__verdict--' + row.outcome" :key="row.id">
+                {{ getSchool(row.outcome) }}
+              </td>
+            </template>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -129,3 +124,45 @@ export default {
 }
 
 </script>
+
+<style scoped lang="scss">
+.history-page {
+  z-index: 0;
+}
+.history-center{
+  position: relative;
+  text-align: center;
+  overflow-x: auto;
+  white-space: nowrap;
+  padding-left: 5vw;
+  width: 95vw;
+}
+.history-table {
+  text-align: center;
+  font-size: .75rem;
+  border-right: 1px solid rgba($text, 0.5);
+  border-collapse: collapse;
+  th, td {
+    padding: 0 5px;
+  }
+  tr {
+    border: 1px solid rgba($text, 0.5);
+    td {
+      border-right: 1px solid rgba($text, 0.5);
+      border-left: 1px solid rgba($text, 0.5);
+    }
+  }
+  .history-table__verdict--nctu {
+    background: $nctu;
+  }
+  .history-table__verdict--nthu {
+    background: $nthu;
+  }
+  .history-table__verdict--stop {
+    background: $stop;
+  }
+  .history-table__verdict--draw {
+    background: $equal;
+  }
+}
+</style>
